8a06be10f56fced054239017dc17ebb34d54bb3b
Heggyhere <heggyy@gmail.com>
2018-10-20 16:41:12 -0700

    render

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@
 	// import styles
 	// render App component into app-root element
 
+	import React from "react"
+	import ReactDOM from "react-dom"
+	import "./style.css"
+	
+	import App from "./App.jsx"
+	
+	ReactDOM.render(
+		<App />,
+		document.getElementById("app-root")
+	)
+	
 
